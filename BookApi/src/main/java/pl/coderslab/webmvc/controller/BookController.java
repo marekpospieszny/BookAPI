39ae875fe95ceaa42de9e2b5696aff87e4648c84
Marek <marek.pospieszny@gmail.com>
2022-03-16 21:45:00 +0100

    Crated BookController

--- BookApi/src/main/java/pl/coderslab/webmvc/controller/BookController.java
+++ BookApi/src/main/java/pl/coderslab/webmvc/controller/BookController.java
@@ -1,62 +1,71 @@
 package pl.coderslab.webmvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.webmvc.model.Book;
 import pl.coderslab.webmvc.model.BookDao;
-import pl.coderslab.webmvc.model.MockBookService;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/books")
 public class BookController {
 
-//    private final MockBookService mockBookService;
     private final BookDao bookDao;
 
-//    @Autowired
-//    public BookController(MockBookService mockBookService) {
-//        this.mockBookService = mockBookService;
-//    }
     @Autowired
     public BookController(BookDao bookDao) {this.bookDao = bookDao;}
 
-//    @RequestMapping("/helloBook")
-//    public Book helloBook() {
-//        return new Book(1, "9788324631766", "Thinking in Java",
-//                "Bruce Eckel", "Helion", "programming");
-//    }
-
     //    GET /books zwraca listę wszystkich książek:
     @GetMapping("")
+    @ResponseBody
     public List<Book> listAllBooks() {
         return bookDao.findAll();
     }
 
-////    GET /books zwraca listę wszystkich książek:
-//    @GetMapping("")
-//    public List<Book> listBooks() {
-//        return mockBookService.getList();
-//    }
+    @GetMapping("/")
+    public String mainMenu(Model model) {
+        model.addAttribute("booksList",bookDao.findAll());
+        return "menu";
+    }
 
 //    GET /books/{id} wyświetla informacje o książce o podanym id:
     @GetMapping("/{id}")
+    @ResponseBody
     public Book getBookById(@PathVariable int id) {
         return bookDao.getBookById(id);
     }
 
-//    @GetMapping("/{id}")
-//    public Book getBookById(@PathVariable int id) {
-//        return mockBookService.getBookById(id);
-//    }
+//     POST /books zapisuje nową książkę przekazaną formularzem do bazy danych
+    @PostMapping("/")
+    public String addBook(@RequestParam String isbn, @RequestParam String title, @RequestParam String author,
+                        @RequestParam String publisher, @RequestParam String type) {
+        Book book = new Book(isbn,title,author,publisher,type);
+        bookDao.create(book);
+        return "redirect:/books/";
+    }
 
-//    @PostMapping("")
-//    public List<Book> addBook(Book book) {
-//        mockBookService.addBook(book);
-//        return mockBookService.getList();
-//    }
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable int id, Model model) {
+        model.addAttribute("book",bookDao.getBookById(id));
+        return "edit";
+    }
 
+    @PostMapping("/edit/{id}")
+    public String editBook(@PathVariable int id, @RequestParam String isbn, @RequestParam String title, @RequestParam String author,
+                           @RequestParam String publisher, @RequestParam String type) {
+        Book book = new Book(id,isbn,title,author,publisher,type);
+        bookDao.update(book);
+        return "redirect:/books/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable int id) {
+        bookDao.delete(id);
+        return "redirect:/books/";
+    }
 
 }
 
